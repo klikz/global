0c21609e4217420de0c0c9ce2fec1a8503b8f910
Sadriddin <musayevsd@mail.ru>
2022-06-22 16:30:12 +0500

    removed material ui

--- src/app/routes/defects/defects.module.ts
+++ src/app/routes/defects/defects.module.ts
@@ -17,17 +17,6 @@ import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 import { MenuModule } from 'primeng/menu';
 
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatDialogModule } from '@angular/material/dialog';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatListModule } from '@angular/material/list';
-// import { MatMenuModule } from '@angular/material/menu';
-// import { MatSelectModule } from '@angular/material/select';
-// import { MatTooltipModule } from '@angular/material/tooltip';
-
-
 @NgModule({
   declarations: [
     DefectsComponent
@@ -46,17 +35,7 @@ import { MenuModule } from 'primeng/menu';
     FormsModule,
     ReactiveFormsModule,
     ZXingScannerModule,
-    MenuModule,
-    // MatDialogModule,
-    // MatListModule,
-    // MatTooltipModule,
-    // MatButtonModule,
-    // MatFormFieldModule,
-    // MatSelectModule,
-    // MatInputModule,
-    // MatMenuModule,
-    // MatIconModule,
-    
+    MenuModule
   ]
 })
 export class DefectsModule { }
