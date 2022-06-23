e27d124cbf528e2b98a9e4e1c9fdcedeb9127a6b
Sadriddin <musayevsd@mail.ru>
2022-06-23 16:16:36 +0500

    added repair process

---
+++ src/app/routes/defects/repair/repair.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+import {IRemont} from 'src/app/models/models'
+import { BackendapiService } from 'src/app/services/backendapi.service'
+
+interface Transport {
+  plates: string;
+  slot: Slot;
+}
+
+interface Slot {
+  name: string;
+  description: string;
+}
+
+
+@Component({
+  selector: 'app-repair',
+  templateUrl: './repair.component.html',
+  styleUrls: ['./repair.component.css']
+})
+export class RepairComponent implements OnInit {
+
+  listRemont: IRemont[]
+  token: string | null
+  selectedId: IRemont
+  someError: boolean = false
+  errorText: string = ""
+  selected: boolean = false
+  processed: boolean = false
+
+  constructor(public api: BackendapiService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.token = localStorage.getItem("token")
+    this.listRemont = await this.api.getRemontList("null", "null", 0, this.token)
+  }
+  async update(){
+    let result = await this.api.updateRemont("null", "null", this.selectedId.id, this.token)
+    if (result.error){
+      this.processed = false
+      this.someError = true
+      this.errorText = result.error
+      return
+    }
+    this.selected = false
+    this.processed = true
+    this.listRemont = await this.api.getRemontList("null", "null", 0, this.token)
+  }
+}
